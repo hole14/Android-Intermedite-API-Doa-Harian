f1d7a1c662d977f658d5037dd3180f35f8bc75de
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-12 13:34:11 +0700

    Menyelesaikan Perpindahan ke data doa

--- app/src/main/java/com/example/apidoa/MainActivity.kt
+++ app/src/main/java/com/example/apidoa/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.apidoa
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -22,12 +23,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         getRandomDoa()
+
+        binding.btnAllDoa.setOnClickListener{
+            val intent = Intent(this@MainActivity, ListDoaActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun getRandomDoa() {
