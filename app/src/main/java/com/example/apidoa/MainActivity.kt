b51e0286358ec905cc4a80e35f6b2fceb44ba3ee
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-12 09:52:04 +0700

    Menambahkan visibility, AsyncHttpClient(), dan url di getRandomDoa dalam MainActivity.kt

--- app/src/main/java/com/example/apidoa/MainActivity.kt
+++ app/src/main/java/com/example/apidoa/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.apidoa
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.apidoa.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -23,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getRandomDoa() {
+        binding.progressBar.visibility = View.VISIBLE
 
+        val client = AsyncHttpClient()
+        val url = "https://doa-doa-api-ahmadramadhan.fly.dev/api/doa/v1/random"
     }
 }
