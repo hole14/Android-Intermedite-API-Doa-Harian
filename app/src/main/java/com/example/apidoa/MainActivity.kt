bbdc7192a300a7c7c18ad28d8c5c3c5222f5073f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-12 09:43:18 +0700

    Menambahkan getRandomDoa di MainActivity.kt

--- app/src/main/java/com/example/apidoa/MainActivity.kt
+++ app/src/main/java/com/example/apidoa/MainActivity.kt
@@ -5,11 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.apidoa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private val TAG = MainActivity::class.java.simpleName
+    }
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getRandomDoa()
+    }
+
+    private fun getRandomDoa() {
 
     }
 }
